1a25db0d4e0ba466842f83a25add116d2165a10c
Androbuddy <anirudhchimpidi8@gmail.com>
2022-05-11 16:53:30 +0530

    Add Top navbar
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>

---
+++ src/components/NavBar.vue
@@ -0,0 +1,10 @@
+<template>
+	<header class="flex p-4 border-b-2 border-slate-200 overflow-y-scroll">
+		<nav>
+			<ul class="flex gap-6">
+				<li>Live Stream</li>
+				<li v-for="i in 2" :key="i">Item {{ i }}</li>
+			</ul>
+		</nav>
+	</header>
+</template>
